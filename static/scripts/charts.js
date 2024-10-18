82679bd2593b5e462501ffca8df57c00b316653d
KyKirma <pedrohenriquekourly@hotmail.com>
2024-10-17 23:50:43 -0300

    "Replaced Chart.js with Google Charts in charts.js and updated HTML template to match"

--- static/scripts/charts.js
+++ static/scripts/charts.js
@@ -1,3 +1,20 @@
+google.charts.load("current", {packages:['corechart', 'line']});
+google.charts.setOnLoadCallback();
+
+function drawChart(info) {
+    console.log(info);
+    var data = google.visualization.arrayToDataTable(info);
+
+    var options = {
+        title: 'Área de Cana Total',
+        curveType: 'none',
+        legend: { position: 'bottom' }
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('myChart'));
+    chart.draw(data, options);
+}
+
 async function getDados() {
     try {
         var response = await fetch('/data/resume/2022');
@@ -11,35 +28,18 @@ async function getDados() {
 
 $(document).ready(function () {
     getDados().then(function (response) {
-        const ctx = document.getElementById('myChart');
+        google.charts.load("current", {packages:["corechart"]});
+        // Cria uma nova array combinando MUNICIPIO e TOTAL_AREA
+        const municipios = Object.values(response.data.MUNICIPIO);
+        const totalArea = Object.values(response.data.TOTAL_AREA);
 
-        new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: Object.values(response.data.MUNICIPIO),
-            datasets: [{
-            label: 'Área total de Cana',
-            data: Object.values(response.data.TOTAL_AREA),
-            borderWidth: 0.5
-            }]
-        },
-        options: {
-            plugins: {
-                title: {
-                    display: true,
-                    text: 'Área Total de Cana',
-                    align: 'center',
-                    color: 'black',
-                    font: {size: 25}
-                }
-            },
-            responsive: true,
-            scales: {
-            y: {
-                beginAtZero: true
-            }
-            }
+        const chartData = [['Município', 'Área']];
+
+        for (let i = 0; i < municipios.length; i++) {
+            chartData.push([municipios[i], totalArea[i]]);
         }
-        });
+
+        console.log(chartData);
+        drawChart(chartData);
     });
 });
